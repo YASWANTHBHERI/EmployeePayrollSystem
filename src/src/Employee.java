0c216f8b5eaedbb58f2af2c797c208f8761f9485
yaswanth <bheriyaswanth@gmail.com>
2024-06-15 11:33:28 +0530

    first commit

---
+++ src/src/Employee.java
@@ -0,0 +1,122 @@
+import java.util.ArrayList;
+abstract class Employee {
+
+    private String name;
+    private int id;
+
+    public Employee(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getId(){
+        return id;
+    }
+
+    public abstract double calculateSalary();
+
+    @Override
+    public String toString(){
+        return "Employee [name = "+name+", id = "+id+",salary = "+calculateSalary()+"]";
+    }
+
+
+}
+
+class FulltimeEmployee extends Employee{
+
+    private double monthSalary;
+
+    public FulltimeEmployee(String name, int id, double monthSalary ){
+        super(name, id);
+        this.monthSalary = monthSalary;
+    }
+    @Override
+    public double calculateSalary() {
+        return monthSalary;
+    }
+
+}
+
+class PartTimeEmployee extends Employee{
+    private int hoursWorked;
+    private double  hourlyRate;
+
+    public PartTimeEmployee(String name, int id, double hourlyRate, int hoursWorked){
+        super(name, id);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public double calculateSalary(){
+        return hoursWorked * hourlyRate;
+    }
+
+}
+
+class PayrollSystem{
+    public ArrayList<Employee> employeeList;
+
+    public PayrollSystem(){
+        employeeList = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee employee){
+        employeeList.add(employee);
+    }
+
+    public void removeEmployee(int id){
+        Employee employeeToRemove = null;
+
+        for(Employee emp: employeeList){
+            if(emp.getId()==id) {
+                employeeToRemove = emp;
+                break;
+            }
+        }
+
+        if (employeeToRemove!=null) employeeList.remove(employeeToRemove);
+    }
+
+    public void displayAllEmployees(){
+        for(Employee emp: employeeList){
+            System.out.println(emp);
+        }
+    }
+
+    public void displayEmployeeById(int id){
+        Employee getEmployeeById = null;
+        for(Employee emp:employeeList){
+            if(emp.getId()==id)
+            {
+                System.out.println(emp);
+                getEmployeeById = emp;
+                break;
+            }
+        }
+        if(getEmployeeById==null) System.out.println("No Employee Found");
+    }
+
+    public void displayMultipleEmployeesByName(String name){
+        ArrayList<Employee> multipleEmployees = new ArrayList<>();
+        for (Employee emp : employeeList){
+            String empName = name.toUpperCase();
+            if(emp.getName().toUpperCase().equals(empName)){
+                multipleEmployees.add(emp);
+            }
+        }
+        if(multipleEmployees.size()==0) System.out.println("No Employees Found");
+        else{
+            for (Employee emp : multipleEmployees){
+                System.out.println(emp);
+            }
+        }
+    }
+
+}
+
+
---
+++ src/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        //creating objects
+        PayrollSystem payrollSystem = new PayrollSystem();
+        FulltimeEmployee emp1 = new FulltimeEmployee("yaswanth",1,50000.0);
+        PartTimeEmployee emp2 = new PartTimeEmployee("Yash",2,100,40);
+        FulltimeEmployee emp3 = new FulltimeEmployee("yaswanth",3,70000.0);
+        PartTimeEmployee emp4 = new PartTimeEmployee("Yash",4,100,60);
+        FulltimeEmployee emp5 = new FulltimeEmployee("yaswanth",5,90000.0);
+        PartTimeEmployee emp6 = new PartTimeEmployee("Niranjan",6,100,80);
+
+
+        //adding employees in employeeList
+
+        payrollSystem.addEmployee(emp1);
+        payrollSystem.addEmployee(emp2);
+        payrollSystem.addEmployee(emp3);
+        payrollSystem.addEmployee(emp4);
+        payrollSystem.addEmployee(emp5);
+        payrollSystem.addEmployee(emp6);
+
+        //displaying employees
+        System.out.println("All Employees");
+        payrollSystem.displayAllEmployees();
+        System.out.println("Removing Employee");
+        payrollSystem.removeEmployee(2);
+        System.out.println("After Employee Removed");
+        payrollSystem.displayAllEmployees();
+        System.out.println("Getting Employee with Id");
+        payrollSystem.displayEmployeeById(1);
+        System.out.println("Getting Multiple Employees");
+        payrollSystem.displayMultipleEmployeesByName("Yaswanth");
+    }
+}
